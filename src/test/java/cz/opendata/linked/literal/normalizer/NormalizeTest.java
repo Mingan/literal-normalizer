4e8bb3dee369d3b45b22e0e14c09612b7af57f3a
Štěpán Pilař <ja@mingan.name>
2014-03-22 08:54:59 +0100

    Refactored test

--- src/test/java/cz/opendata/linked/literal/normalizer/NormalizeTest.java
+++ src/test/java/cz/opendata/linked/literal/normalizer/NormalizeTest.java
@@ -5,7 +5,9 @@ import cz.cuni.mff.xrg.odcs.rdf.enums.RDFFormatType;
 import cz.cuni.mff.xrg.odcs.rdf.exceptions.CannotOverwriteFileException;
 import cz.cuni.mff.xrg.odcs.rdf.exceptions.RDFException;
 import cz.cuni.mff.xrg.odcs.rdf.interfaces.RDFDataUnit;
+import org.junit.Before;
 import org.junit.Test;
+import org.openrdf.model.Statement;
 import org.openrdf.rio.RDFFormat;
 
 import java.util.LinkedList;
@@ -16,13 +18,28 @@ import static org.junit.Assert.assertTrue;
 
 public class NormalizeTest {
 
+    public static final String OUTPUT_DIR = "w:\\dip\\tmp\\";
+
+    private Transformer transformer;
+    private TransformerConfig config;
+    private TestEnvironment env;
+    private RDFDataUnit input;
+    private RDFDataUnit normalized;
+
+    @Before
+    public void prepare() {
+        transformer = new Transformer();
+        config = new TransformerConfig();
+        env = TestEnvironment.create();
+        input = null;
+        normalized = null;
+    }
+
     @Test
     public void wholeStringsTest() throws Exception {
-        // prepare dpu instance and configure it
-        Transformer transformer = new Transformer();
-        TransformerConfig config = new TransformerConfig();
-
+        // setup transformation
         String replacement = "CZK";
+        config.setReplacement(replacement);
 
         List<String> list = new LinkedList<>();
         list.add("czk");
@@ -30,59 +47,60 @@ public class NormalizeTest {
         list.add("kč");
         list.add("Kč");
         config.setToMatch(list);
-        config.setReplacement(replacement);
+
         transformer.configureDirectly(config);
 
-
-        TestEnvironment env = TestEnvironment.create();
-
-
-        RDFDataUnit input = null;
-        RDFDataUnit normalized = null;
-        try {
-            input = env.createRdfInputFromResource(
-                    "input", false,
-                    "whole-strings.ttl", RDFFormat.TURTLE);
-            normalized = env.createRdfOutput("output", false);
-        } catch (RDFException e) {
-            e.printStackTrace();
-        }
+        // setup data units
+        loadDataFromFile("whole-strings");
+        normalized = env.createRdfOutput("output", false);
 
         assertTrue(input.getTripleCount() > 0);
+
+
         try {
-            // run the execution
             env.run(transformer);
 
-            int matches = 0;
-            int expectedMatches = 4;
-            List<org.openrdf.model.Statement> triples = normalized.getTriples();
-            for (int i = 0; i < triples.size(); i++) {
-                System.out.println(triples.get(i).getObject().stringValue() + " " + replacement);
-                System.out.println(triples.get(i).getObject().stringValue() == replacement.toString());
-                if (triples.get(i).getObject().stringValue() == replacement) {
-                    matches += 1;
-                }
-            }
+            expectExactMatches(replacement, 4);
 
+            printResultToFile("whole-strings");
 
-            try {
-                Random rg = new Random();
-                normalized.loadToFile("w:\\dip\\tmp\\whole-strings-" + rg.nextInt() + ".ttl", RDFFormatType.TTL);
-            } catch (CannotOverwriteFileException e) {
-                e.printStackTrace();
-            } catch (RDFException e) {
-                e.printStackTrace();
-            }
-
-            System.out.println("Value matched " + matches + " of " + expectedMatches + " times");
-            // verify result
             assertTrue(input.getTripleCount() == normalized.getTripleCount());
-            assertTrue(matches == expectedMatches);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            // release resources
             env.release();
+        }
+    }
+
+    private void expectExactMatches(String replacement, int expectedMatches) {
+        int matches = 0;
+        List<Statement> triples = normalized.getTriples();
+        for (int i = 0; i < triples.size(); i++) {
+            if (triples.get(i).getObject().stringValue().equals(replacement)) {
+                matches += 1;
+            }
+        }
+        assertTrue(matches == expectedMatches);
+    }
+
+    private void printResultToFile(String file) {
+        try {
+            Random rg = new Random();
+            normalized.loadToFile(OUTPUT_DIR + file + "-" + rg.nextInt() + ".ttl", RDFFormatType.TTL);
+        } catch (CannotOverwriteFileException e) {
+            e.printStackTrace();
+        } catch (RDFException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadDataFromFile(String file) {
+        try {
+            input = env.createRdfInputFromResource(
+                    "input", false,
+                    file + ".ttl", RDFFormat.TURTLE);
+        } catch (RDFException e) {
+            e.printStackTrace();
         }
     }
 }
